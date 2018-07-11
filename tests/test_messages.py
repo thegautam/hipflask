9a36e3e83bcc61089f24109c262349a0d3f8ca00
Gautam Kedia <gautamkedia@gmail.com>
2018-07-10 23:45:01 -0700

    Refactoring test and adding failing weather test.

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -10,19 +10,44 @@ from hipflask.app import setup_app
 from hipflask.messages import Message
 
 SUCCESS_CODE = 200
+API = '/chat/messages'
 
-def test_user_join():
+
+@pytest.fixture
+def app():
+    ''' Create app for use in all tests '''
     app, _ = setup_app()
-    url = '/chat/messages'
+    return app
+
+
+def assert_text_message(result, message):
+    ''' Helper method to test text message success response '''
+    assert result.status_code == SUCCESS_CODE, 'Invalid status'
+    data = json.loads(result.data)
+    messages = data['messages']
+    assert messages[0]['type'] == 'text', 'Invalid type'
+    assert messages[0]['text'] == message, 'Invalid greeting'
+
+
+def test_user_join(app):
+    ''' User join should result in greeting '''
     name = "John"
     payload = {
         "action": "join",
         "user_id": 123456,
         "name": name
     }
-    result = app.test_client().post(url, data=payload)
-    assert result.status_code == SUCCESS_CODE, 'Invalid status'
-    data = json.loads(result.data)
-    messages = data['messages']
-    assert messages[0]['type'] == 'text', 'Invalid type'
-    assert messages[0]['text'] == 'Hello, {}!'.format(name), 'Invalid greeting'
+    result = app.test_client().post(API, data=payload)
+    assert_text_message(result, 'Hello, {}!'.format(name))
+
+
+def test_weather(app):
+    city = "Seattle"
+    payload = {
+        "action": "message",
+        "user_id": 123456,
+        "text": "Weather in {}".format(city)
+    }
+    result = app.test_client().post(API, data=payload)
+    assert_text_message(result, 'Hello, {}!'.format(city))
+
