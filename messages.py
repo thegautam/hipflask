03d652004a3a9bc8f6476b9d583ba52f34a72fd6
Gautam Kedia <gautamkedia@gmail.com>
2018-07-10 22:59:00 -0700

    Handling join case successfully.

--- messages.py
+++ messages.py
@@ -2,7 +2,24 @@ from flask import request
 from flask_restful import Resource
 
 class Message(Resource):
+
+    def _error_response(self, message):
+        return { "error": message }, 400
+
+    def _text_response(self, message):
+        return { "messages":[{ "type": "text", "text": message }]}, 200
+
     def post(self):
         data = request.form
-        output = "message: " + data['text']
-        return { "messages":[{ "type":"text", "text":output }]}, 201
+
+        # Validate payload is correct
+        action = data.get('action')
+        if not action or not action in ['join', 'message']:
+            return self._error_response('Invalid action')
+
+        if action == 'join':
+            name = data['name']
+            return self._text_response('Hello, {}!'.format(name))
+        else:
+            output = "message: " + data['text']
+            return { "messages":[{ "type":"text", "text":output }]}, 200
--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,14 +1,15 @@
-import sys
+import json
 import os
+import sys
 import tempfile
-import pytest
 
+import pytest
 from flask import Flask, request
 
-from hipflask.messages import Message
 from hipflask.app import setup_app
+from hipflask.messages import Message
 
-SUCCESS_CODE = 201
+SUCCESS_CODE = 200
 
 def test_user_join():
     app, _ = setup_app()
@@ -21,5 +22,7 @@ def test_user_join():
     }
     result = app.test_client().post(url, data=payload)
     assert result.status_code == SUCCESS_CODE, 'Invalid status'
-    assert result.data.messages[0].type == 'text', 'Invalid type'
-    assert result.data.messages[0].text == 'Hello, {}!'.format(name), 'Invalid greeting'
+    data = json.loads(result.data)
+    messages = data['messages']
+    assert messages[0]['type'] == 'text', 'Invalid type'
+    assert messages[0]['text'] == 'Hello, {}!'.format(name), 'Invalid greeting'
